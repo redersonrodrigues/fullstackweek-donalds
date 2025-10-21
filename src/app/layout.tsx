20649686ae448035a1977b3242f55ad28edb76ec
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2025-10-20 23:03:06 -0300

    feat: add initial finish order dialog components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
-
 import "./globals.css";
 
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+
+import { Toaster } from "@/components/ui/sonner";
+
+import { CartProvider } from "./[slug]/menu/contexts/cart";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -21,7 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+      <body className={`${poppins.className} antialiased`}>
+        <CartProvider>{children}</CartProvider>
+
+        <Toaster/>
+      </body>
     </html>
   );
 }
